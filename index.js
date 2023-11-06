7a02c1fe4533a56ed832c0ef3055796cb0313fda
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-11-06 12:07:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+// Reads the PORT value from the environment variable `PORT`.
+// If not found, uses the default value of 3000.
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
